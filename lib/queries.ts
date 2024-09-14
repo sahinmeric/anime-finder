06f10a94cab8041087169998aab8d93492c1c576
Sahin Meric <sahin.meric@medisprout.com>
2024-09-13 22:18:33 -0500

    get random anime query moved

--- lib/queries.ts
+++ lib/queries.ts
@@ -19,3 +19,21 @@ export const GET_SEARCHED_ANIME = gql`
     }
   }
 `;
+
+export const GET_RANDOM_ANIME = gql`
+  query GetRandomAnime {
+    Page(page: 1, perPage: 5) {
+      media {
+        id
+        title {
+          romaji
+          english
+        }
+        coverImage {
+          large
+        }
+        description
+      }
+    }
+  }
+`;
