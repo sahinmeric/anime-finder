80f2a21ca6131e5cc88c5b95dac91766fb1aec3a
Sahin Meric <sahin.meric@medisprout.com>
2024-09-21 13:01:48 -0500

    Added new filters, hero removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,39 @@
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { GET_SEARCHED_ANIME } from "@/lib/queries";
-import Hero from "./Hero";
-import SearchForm from "./SearchForm";
-import SearchResults from "./SearchResults";
+import Filters from "./Filters";
+import { GET_ANIME_BY_FILTERS } from "@/lib/queries";
 
 const Home = () => {
-  const [submittedTerm, setSubmittedTerm] = useState("");
+  const [genre, setGenre] = useState("");
+  const [year, setYear] = useState("");
+  const [season, setSeason] = useState("");
+  const [format, setFormat] = useState("");
 
-  const { data, loading, error } = useQuery(GET_SEARCHED_ANIME, {
-    variables: { search: submittedTerm },
-    skip: !submittedTerm,
+  const { data, loading, error } = useQuery(GET_ANIME_BY_FILTERS, {
+    variables: {
+      genre: genre !== "" && genre !== "Any" ? [genre] : null,
+      year: year !== "" ? parseInt(year) : null,
+      season: season !== "" && season !== "Any" ? season : null,
+      format: format !== "" && format !== "Any" ? format : null,
+    },
+    skip: !genre && !year && !season && !format,
   });
-
-  const handleSearch = (term: string) => {
-    setSubmittedTerm(term);
-  };
 
   return (
     <div className="container mx-auto">
-      <Hero />
-      <SearchForm onSearch={handleSearch} />
-      <SearchResults data={data} loading={loading} error={error} />
+      <Filters
+        genre={genre}
+        year={year}
+        season={season}
+        format={format}
+        setGenre={setGenre}
+        setYear={setYear}
+        setSeason={setSeason}
+        setFormat={setFormat}
+      />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && console.log(data)}
     </div>
   );
 };
