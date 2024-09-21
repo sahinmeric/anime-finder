699c2897e96d3e9620b163132fb8a8a09d57d063
Sahin Meric <sahin.meric@medisprout.com>
2024-09-21 13:50:27 -0500

    constants created

---
+++ lib/constants.ts
@@ -0,0 +1,49 @@
+export const GENRES = [
+  "Action",
+  "Adventure",
+  "Comedy",
+  "Drama",
+  "Ecchi",
+  "Fantasy",
+  "Horror",
+  "Mahou Shoujo",
+  "Mecha",
+  "Music",
+  "Mystery",
+  "Psychological",
+  "Romance",
+  "Sci-Fi",
+  "Slice of Life",
+  "Sports",
+  "Supernatural",
+  "Thriller",
+];
+
+export const YEARS = [
+  "2025",
+  "2024",
+  "2023",
+  "2022",
+  "2021",
+  "2020",
+  "2019",
+  "2018",
+  "2017",
+  "2016",
+  "2015",
+  "2014",
+  "2013",
+  "2012",
+];
+
+export const SEASONS = ["WINTER", "SPRING", "SUMMER", "FALL"];
+
+export const FORMATS = [
+  { label: "TV Show", value: "TV" },
+  { label: "Movie", value: "MOVIE" },
+  { label: "TV Short", value: "TV_SHORT" },
+  { label: "Special", value: "SPECIAL" },
+  { label: "OVA", value: "OVA" },
+  { label: "ONA", value: "ONA" },
+  { label: "Music", value: "MUSIC" },
+];
