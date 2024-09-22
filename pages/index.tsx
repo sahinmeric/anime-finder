bfba039fdd627fd7ccc175eb29819ed3a5deee4c
Sahin Meric <sahin.meric@medisprout.com>
2024-09-22 08:46:50 -0500

    added search by name added, search result added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,62 @@
 import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import Filters from "./Filters";
-import { GET_ANIME_BY_FILTERS } from "@/lib/queries";
+import { GET_ANIME_BY_FILTERS, GET_SEARCHED_ANIME } from "@/lib/queries";
+import SearchResults from "./SearchResults";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [genre, setGenre] = useState("");
   const [year, setYear] = useState("");
   const [season, setSeason] = useState("");
   const [format, setFormat] = useState("");
 
-  const { data, loading, error } = useQuery(GET_ANIME_BY_FILTERS, {
+  const {
+    data: filterData,
+    loading: filterLoading,
+    error: filterError,
+  } = useQuery(GET_ANIME_BY_FILTERS, {
     variables: {
+      search: searchTerm !== "" ? searchTerm : null,
       genre: genre !== "" && genre !== "Any" ? [genre] : null,
       year: year !== "" ? parseInt(year) : null,
       season: season !== "" && season !== "Any" ? season : null,
       format: format !== "" && format !== "Any" ? format : null,
     },
-    skip: !genre && !year && !season && !format,
+    skip: !!searchTerm,
   });
 
+  const {
+    data: searchData,
+    loading: searchLoading,
+    error: searchError,
+  } = useQuery(GET_SEARCHED_ANIME, {
+    variables: { search: searchTerm, sort: ["SEARCH_MATCH"] },
+    skip: !searchTerm,
+  });
+
+  console.log({ searchTerm, genre, year, season, format });
+
   return (
-    <div className="container mx-auto">
+    <>
       <Filters
+        searchTerm={searchTerm}
         genre={genre}
         year={year}
         season={season}
         format={format}
+        setSearchTerm={setSearchTerm}
         setGenre={setGenre}
         setYear={setYear}
         setSeason={setSeason}
         setFormat={setFormat}
       />
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {data && console.log(data)}
-    </div>
+      <SearchResults
+        data={searchData ?? filterData}
+        loading={searchLoading ?? filterLoading}
+        error={searchError ?? filterError}
+      />
+    </>
   );
 };
 
