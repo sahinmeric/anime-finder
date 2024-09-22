718558d62f4daa5e951b0370e6c13354403314fe
Sahin Meric <sahin.meric@medisprout.com>
2024-09-22 10:53:51 -0500

    resolved Cannot read properties of undefined error

--- pages/AnimeCard.tsx
+++ pages/AnimeCard.tsx
@@ -6,15 +6,14 @@ interface AnimeCardProps {
 }
 
 const AnimeCard: React.FC<AnimeCardProps> = ({ anime }) => {
+  const placeholderImage = "/img/placeholder/placeholder_no_image.jpg"; // Placeholder image path from public folder
+
   return (
     <div className="anime-card bg-white shadow-lg rounded-lg overflow-hidden flex flex-col justify-between h-full">
       <div className="flex-grow">
         <Image
-          src={
-            anime.coverImage?.large ||
-            "/img/placeholder/placeholder_no_image.jpg"
-          }
-          alt={anime.title.english || anime.title.romaji}
+          src={anime?.coverImage?.large || placeholderImage} // Use placeholder image if coverImage is undefined
+          alt={anime?.title?.english || anime?.title?.romaji || "Unknown Anime"}
           width={300}
           height={400}
           className="w-full h-80 object-cover"
@@ -22,7 +21,7 @@ const AnimeCard: React.FC<AnimeCardProps> = ({ anime }) => {
       </div>
       <div className="p-4 flex flex-col justify-end">
         <h3 className="text-md text-gray-800 text-center mb-4">
-          {anime.title.english || anime.title.romaji}
+          {anime?.title?.english || anime?.title?.romaji || "Unknown Title"}
         </h3>
       </div>
     </div>
