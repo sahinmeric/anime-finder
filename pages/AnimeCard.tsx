7eb3b0f9a80759c17973e9e4126c5e1787b752b4
Sahin Meric <sahin.meric@medisprout.com>
2024-09-22 09:35:38 -0500

    Anime card UI improvements

---
+++ pages/AnimeCard.tsx
@@ -0,0 +1,29 @@
+import { Anime } from "@/lib/types";
+import Image from "next/image";
+
+interface AnimeCardProps {
+  anime: Anime;
+}
+
+const AnimeCard: React.FC<AnimeCardProps> = ({ anime }) => {
+  return (
+    <div className="anime-card bg-white shadow-lg rounded-lg overflow-hidden flex flex-col justify-between h-full">
+      <div className="flex-grow">
+        <Image
+          src={anime.coverImage.large}
+          alt={anime.title.english || anime.title.romaji}
+          width={300}
+          height={400}
+          className="w-full h-80 object-cover"
+        />
+      </div>
+      <div className="p-4 flex flex-col justify-end">
+        <h3 className="text-md text-gray-800 text-center mb-4">
+          {anime.title.english || anime.title.romaji}
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default AnimeCard;
