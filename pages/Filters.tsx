2cdbcf72e0c859d6cbf5ad3c82203e34b7334953
Sahin Meric <sahin.meric@medisprout.com>
2024-09-22 08:45:56 -0500

    added search filter

--- pages/Filters.tsx
+++ pages/Filters.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { GENRES, YEARS, SEASONS, FORMATS } from "@/lib/constants";
 
 interface FiltersProps {
+  searchTerm: string;
   genre: string;
   year: string;
   season: string;
   format: string;
+  setSearchTerm: (term: string) => void;
   setGenre: (genre: string) => void;
   setYear: (year: string) => void;
   setSeason: (season: string) => void;
@@ -13,85 +15,104 @@ interface FiltersProps {
 }
 
 const Filters = ({
+  searchTerm,
   genre,
   year,
   season,
   format,
+  setSearchTerm,
   setGenre,
   setYear,
   setSeason,
   setFormat,
 }: FiltersProps) => {
   return (
-    <div className="filter-container grid grid-cols-2 md:grid-cols-4 gap-4 mb-4">
-      {/* Genre Filter */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Genre</label>
-        <select
-          value={genre}
-          onChange={(e) => setGenre(e.target.value)}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">Any</option>
-          {GENRES.map((g) => (
-            <option key={g} value={g}>
-              {g}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div className="flex justify-center">
+      <div className="filter-container grid grid-cols-2 md:grid-cols-5 gap-6 my-6 p-4 rounded-lg">
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Search
+          </label>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search anime..."
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
 
-      {/* Year Filter */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Year</label>
-        <select
-          value={year}
-          onChange={(e) => setYear(e.target.value)}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">Any</option>
-          {YEARS.map((y) => (
-            <option key={y} value={y}>
-              {y}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Genre
+          </label>
+          <select
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+            className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">Any</option>
+            {GENRES.map((g) => (
+              <option key={g} value={g}>
+                {g}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      {/* Season Filter */}
-      <div>
-        <label className="block text-sm font-medium">Season</label>
-        <select
-          value={season}
-          onChange={(e) => setSeason(e.target.value)}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">Any</option>
-          {SEASONS.map((s) => (
-            <option key={s} value={s}>
-              {s}
-            </option>
-          ))}
-        </select>
-      </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Year
+          </label>
+          <select
+            value={year}
+            onChange={(e) => setYear(e.target.value)}
+            className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">Any</option>
+            {YEARS.map((y) => (
+              <option key={y} value={y}>
+                {y}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      {/* Format Filter */}
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Format
-        </label>
-        <select
-          value={format}
-          onChange={(e) => setFormat(e.target.value)}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-        >
-          <option value="">Any</option>
-          {FORMATS.map((f) => (
-            <option key={f.value} value={f.value}>
-              {f.label}
-            </option>
-          ))}
-        </select>
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Season
+          </label>
+          <select
+            value={season}
+            onChange={(e) => setSeason(e.target.value)}
+            className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">Any</option>
+            {SEASONS.map((s) => (
+              <option key={s} value={s}>
+                {s}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Format
+          </label>
+          <select
+            value={format}
+            onChange={(e) => setFormat(e.target.value)}
+            className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          >
+            <option value="">Any</option>
+            {FORMATS.map((f) => (
+              <option key={f.value} value={f.value}>
+                {f.label}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </div>
   );
